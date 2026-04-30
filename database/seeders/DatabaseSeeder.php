ea62d0cb311fed9c3479c493721286001fe018ff
ormeonin <abhilash@ormeon.com>
2026-04-30 09:05:40 +0530

    Register LiveKerala category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,6 +34,7 @@ class DatabaseSeeder extends Seeder
             ContentDetailsSeeder::class,
             PageDetailsSeeder::class,
             KeralaDistrictSeeder::class,
+            LiveKeralaCategorySeeder::class,
 
         ]);
     }
