dc774e4972eb6b6592e78ab0f72ca422b8bc622d
ormeonin <abhilash@ormeon.com>
2026-04-30 15:04:38 +0530

    Fix custom menu links for internal paths and safer updates

--- app/Http/Controllers/Admin/ManageMenuController.php
+++ app/Http/Controllers/Admin/ManageMenuController.php
@@ -76,7 +76,16 @@ class ManageMenuController extends Controller
         $selectedTheme = basicControl()->theme;
         $rules = [
             'link_text' => 'required|string|min:2|max:100',
-            'link' => 'required|url',
+            'link' => [
+                'required',
+                'string',
+                'max:255',
+                function ($attribute, $value, $fail) {
+                    if (!$this->isValidCustomLink($value)) {
+                        $fail('The link must be a valid URL or start with /.');
+                    }
+                },
+            ],
         ];
 
         $validator = Validator::make($request->all(), $rules);
@@ -87,11 +96,12 @@ class ManageMenuController extends Controller
         }
 
         try {
+            $linkText = trim($request->link_text);
             $pageForMenu = Page::create([
-                'name' => strtolower($request->link_text),
-                'slug' => slug($request->link_text),
+                'name' => strtolower($linkText),
+                'slug' => $this->generateUniqueSlug($linkText),
                 'template_name' => $selectedTheme,
-                'custom_link' => $request->link,
+                'custom_link' => $this->normalizeCustomLink($request->link),
                 'type' => 3
             ]);
 
@@ -102,7 +112,7 @@ class ManageMenuController extends Controller
             $defaultLanguage = Language::where('default_status', true)->first();
             $pageForMenu->details()->create([
                 'language_id' => $defaultLanguage->id,
-                'name' => $request->link_text,
+                'name' => $linkText,
             ]);
 
             return back()->with('success', 'Custom link added to the menu.');
@@ -119,7 +129,15 @@ class ManageMenuController extends Controller
         $rules = [
             'language_id' => 'required',
             'link_text.*' => 'required|max:100',
-            'link.*' => 'required|max:100',
+            'link.*' => [
+                'required',
+                'max:255',
+                function ($attribute, $value, $fail) {
+                    if (!$this->isValidCustomLink($value)) {
+                        $fail('The link must be a valid URL or start with /.');
+                    }
+                },
+            ],
         ];
 
         $message = [
@@ -139,17 +157,16 @@ class ManageMenuController extends Controller
         try {
             $customPage = Page::findOrFail($id);
             $response = $customPage->update([
-                'custom_link' => $request->link[$language]
+                'custom_link' => $this->normalizeCustomLink($request->link[$language])
             ]);
 
             throw_if(!$response, 'Something went wrong while updating custom menu data.');
 
-            if ($language != 0) {
-                $pageDetails = PageDetail::updateOrCreate(
-                    ['page_id' => $id, 'language_id' => $language],
-                    ['page_id' => $id, 'language_id' => $language, 'name' => $request->link_text[$language]]
-                );
-            }
+            $pageDetails = PageDetail::updateOrCreate(
+                ['page_id' => $id, 'language_id' => $language],
+                ['page_id' => $id, 'language_id' => $language, 'name' => $request->link_text[$language]]
+            );
+
             throw_if(!$pageDetails, 'Something went wrong while updating custom menu data.');
 
             return back()->with('success', 'Custom menu updated successfully.');
@@ -168,13 +185,17 @@ class ManageMenuController extends Controller
 
         $lookingKey = $customPage->name;
 
-        $headerMenu->update([
-            'menu_items' => filterCustomLinkRecursive($headerMenu->menu_items, $lookingKey)
-        ]);
+        if ($headerMenu) {
+            $headerMenu->update([
+                'menu_items' => filterCustomLinkRecursive($headerMenu->menu_items, $lookingKey)
+            ]);
+        }
 
-        $footerMenu->update([
-            'menu_items' => filterCustomLinkRecursive($footerMenu->menu_items, $lookingKey)
-        ]);
+        if ($footerMenu) {
+            $footerMenu->update([
+                'menu_items' => filterCustomLinkRecursive($footerMenu->menu_items, $lookingKey)
+            ]);
+        }
 
         $customPage->delete();
         return back()->with('success', 'Custom link deleted from the menu.');
@@ -190,9 +211,57 @@ class ManageMenuController extends Controller
             ->where('language_id', $languageId)
             ->first();
 
+        if (!$customPage) {
+            $defaultLanguage = Language::where('default_status', true)->first();
+            $customPage = PageDetail::with('page:id,name,custom_link')
+                ->where('page_id', $pageId)
+                ->where('language_id', optional($defaultLanguage)->id)
+                ->first();
+        }
+
         return response()->json([
             'name' => $customPage ? $customPage->name : '',
             'custom_link' => $customPage ? optional($customPage->page)->custom_link : ''
         ]);
+    }
+
+    protected function isValidCustomLink(?string $value): bool
+    {
+        $value = trim((string) $value);
+
+        if ($value === '') {
+            return false;
+        }
+
+        if (filter_var($value, FILTER_VALIDATE_URL)) {
+            return true;
+        }
+
+        return str_starts_with($value, '/');
+    }
+
+    protected function normalizeCustomLink(string $value): string
+    {
+        $value = trim($value);
+        if (filter_var($value, FILTER_VALIDATE_URL)) {
+            return $value;
+        }
+
+        return '/' . ltrim($value, '/');
+    }
+
+    protected function generateUniqueSlug(string $linkText): string
+    {
+        $slug = slug($linkText);
+        $slug = $slug ?: 'custom-link';
+        $originalSlug = $slug;
+        $counter = 1;
+
+        while (Page::where('slug', $slug)->exists()) {
+            $slug = $originalSlug . '-' . $counter;
+            $counter++;
+        }
+
+        return $slug;
     }
 }
